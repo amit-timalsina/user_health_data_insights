81d30a339eb173ccfcd7226c9750a27e1b311286
Amit Timalsina <amittimalsina14@gmail.com>
2025-05-07 22:59:10 +0545

    Update init of the test models subpackage

--- rolling_insights/tests/models/__init__.py
+++ rolling_insights/tests/models/__init__.py
@@ -1,5 +1,11 @@
-"""Test models for Rolling Insights."""
+"""Test for pydantic models in the models sub-package."""
 
 from .test_raw_data import TestRawDataPoint
+from .test_metrics import TestHealthMetrics, TestPhoneMetrics, TestSleepMetrics
 
-__all__ = ("TestRawDataPoint",)
+__all__ = (
+    "TestRawDataPoint",
+    "TestHealthMetrics",
+    "TestPhoneMetrics",
+    "TestSleepMetrics",
+)
